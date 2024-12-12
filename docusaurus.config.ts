9c598406a21ca76ff56a924819e2451616970fc5
Abhishek Chatterjee <abhishekchatterjeejit@gmail.com>
2024-12-12 21:50:33 +0530

    Updated deployment script

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "imdeepmind", // Usually your GitHub org/user name.
   projectName: "imdeepmind.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
