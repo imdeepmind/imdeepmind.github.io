113229a1625364ad568574e722b647eb8df2e8cc
Abhishek Chatterjee <abhishekchatterjeejit@gmail.com>
2024-12-16 21:20:12 +0530

    Updated the edit links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/imdeepmind/imdeepmind.github.io/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/imdeepmind/imdeepmind.github.io/blob/main",
         },
         sitemap: {
           changefreq: "weekly",
